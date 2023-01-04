260dded20ede0106ba4c882cf7ff33fca12613a9
BensonTerra <benson.terra@hotmail.com>
2023-01-04 17:18:38 +0000

    aaaaaaaa

--- trabalho.py
+++ trabalho.py
@@ -1,9 +1,5 @@
 """
-Implemente um simulador de Índice de Massa Corporal, em que IMC = massa / (altura * altura) 
-
-frame4 = LabelFrame(frame2,width = 390,height =100)
-frame4.place(x=5,y=5)
-
+doc string
 """
 
 #Bibloiotecas inportadasa inicio
@@ -16,9 +12,15 @@ from tkinter import filedialog
 from PIL import ImageTk,Image    # Imagens .jpg ou .png
 #Bibloiotecas inportadads fim
 
+#Arquivos Funções externas inicio
+from login import *
+
+#Arquivos Funções externas fim
+
 #Variaveis globais inicio
-pasta = "trabalho/files"
-ficheiro = "trabalho/files/acessos.txt" #Numero;data_sistema;hora_sistema;tipo_acesso
+pasta = "files"
+ficheiro = "files/acessos.txt" 
+login = 0
 #Variaveis globais fim
 
 #Verfica a existencia da pasta inicio
@@ -26,12 +28,61 @@ if not os.path.exists(pasta):
     os.mkdir(pasta)
 #Verfica a existencia da pasta fim
 
-def teste():
-    print("teste")
+#tela Area Pessoal inicio
+def telaAreaPessoal():
+    global login
+    global img
+    if login==0:
+        panelProvas = PanedWindow(window, width = 400, height= 450)
+        panelProvas.place(x=300, y= 0)
+
+        ctnImagemPerfil = Canvas(panelProvas,width=60,height=60)
+        ctnImagemPerfil.place(x=10,y=10)
+        img = PhotoImage(file="imagens/person_50x50.png")
+        ctnImagemPerfil.create_image(30, 30, anchor= "center", image = img)
+
+        txtInfo = Label(panelProvas, width = 0, height = 3, text = "Primeiro Nome:\n             Genero:\n   Tipo de conta:", font = ("arial", 12))
+        txtInfo.place(x = 80,y = 10)
+        
+        name = StringVar()
+        name.set("desconhecido")
+        txtNome = Entry(panelProvas, width=20,justify=CENTER,state=DISABLED, textvariable=name)
+        txtNome.place(x=195, y = 10)
+        
+        gender = StringVar()
+        gender.set("outro")
+        txtGenero = Entry(panelProvas, width=20,justify=CENTER,state=DISABLED, textvariable=gender)
+        txtGenero.place(x=195, y = 30)
+
+        type = StringVar()
+        type.set("desconhecido")
+        txtConta= Entry(panelProvas, width=20,justify=CENTER,state=DISABLED, textvariable=type)
+        txtConta.place(x=195, y = 50)
+        
+
+
+
+    elif login==1:
+        panelProvas = PanedWindow(window,bg= "green", width = 400, height= 450)
+        panelProvas.place(x=300, y= 0)
+
+        ctnImagemPerfil = Canvas(panelProvas,width=60,height=60)
+        ctnImagemPerfil.place(x=10,y=10)
+        img = PhotoImage(file="imagens/person_50x50.png")
+        ctnImagemPerfil.create_image(30, 30, anchor= "center", image = img)
+#tela Area Pessoal fim
+
+#tela Gerir Tarefas inicio
+def telaGerirTarefas():
+    panelProvas = PanedWindow(window,bg= "red", width = 400, height= 450)
+    panelProvas.place(x=300, y= 0)
+
+
+#tela Gerir Tarefas fim
 
 #codigo principal inicio
 window = Tk()
-#Get the current screen width and height
+
 screenWidth = window.winfo_screenwidth()
 screenHeight = window.winfo_screenheight()
 appWidth = 1100                             # tamanho (pixeis) da window a criar
@@ -39,50 +90,50 @@ appHeight = 450
 x = (screenWidth/2) - (appWidth/2)        # posição do canto superior esquerdo da window
 y = (screenHeight/2) - (appHeight/2)
 window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}" .format(appWidth, appHeight, int(x), int(y)))
-window.title("App")
+window.title("Programa")
 #-----------------------------------------------------------------------------------------------------------------------------#
 menubar = Menu(window)
 #-----------------------------------------------------------------------------------------------------------------------------#
 filemenu = Menu(menubar, tearoff=0)
 
 menubar.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Movimentos",)
-filemenu.add_command(label="Consultas",)
+filemenu.add_command(label="Movimentos")
+filemenu.add_command(label="Consultas")
 filemenu.add_command(label="CLS", command= lambda: os.system("cls"))
 filemenu.add_command(label="Exit", command=window.destroy)
 window.config(menu=filemenu)
 
 #-----------------------------------------------------------------------------------------------------------------------------#
-#                                       TELA 1-PRINCIPAL
-#-----------------------------------------------------------------------------------------------------------------------------#
 panel1 = PanedWindow(window, bg = "black",width = 300, height = 450)
 panel1.place(x=0 , y=0)
-#-----------------------------------------------------------------------------------------------------------------------------#
-imageIco1 = PhotoImage(file = "trabalho/imagens/person_50x50.png" )
+#Area pessoal-----------------------------------------------------------------------------------------------------------------#
+imageIco1 = PhotoImage(file = "imagens/person_50x50.png" )
 btnOpcao1 = Button(panel1, text = "Area\nPessoal", image = imageIco1, compound=LEFT, relief = "sunken", 
                     width = 200, height = 70, font="calibri, 11",
-                    command=lambda: print("btnOpcao1"))
+                    command=telaAreaPessoal)
 btnOpcao1.place (x=40, y=5)
 #-----------------------------------------------------------------------------------------------------------------------------#
-imageIco2 = PhotoImage(file = "trabalho/imagens/person_50x50.png" )
+imageIco2 = PhotoImage(file = "imagens/person_50x50.png" )
 btnOpcao2 = Button(panel1, text = "Gerir\ntarefas", image = imageIco2, compound=LEFT, relief = "sunken", 
                     width = 200, height = 70, font="calibri, 11",
-                    command=lambda: print("btnOpcao2"))
+                    command=telaGerirTarefas)
 btnOpcao2.place (x=40, y=95)
+"""
 #-----------------------------------------------------------------------------------------------------------------------------#
-imageIco3 = PhotoImage(file = "trabalho/imagens/person_50x50.png" )
+imageIco3 = PhotoImage(file = "imagens/person_50x50.png" )
 btnOpcao3 = Button(panel1, text = "Consultar\ntarefas", image = imageIco3, compound=LEFT, relief = "sunken", 
                     width = 200, height = 70, font="calibri, 11",
                     command=lambda: print("btnOpcao3"))
 btnOpcao3.place (x=40, y=185)
 #-----------------------------------------------------------------------------------------------------------------------------#
-imageIco4 = PhotoImage(file = "trabalho/imagens/person_50x50.png" )
+imageIco4 = PhotoImage(file = "imagens/person_50x50.png" )
 btnOpcao4 = Button(panel1, text = "Admin", image = imageIco4, compound=LEFT, relief = "sunken", 
                     width = 200, height = 70, font="calibri, 11",
                     command=lambda: print("btnOpcao4"))
 btnOpcao4.place (x=40, y=275)
+"""
 #-----------------------------------------------------------------------------------------------------------------------------#
-imageIco5 = PhotoImage(file = "trabalho/imagens/person_50x50.png" )
+imageIco5 = PhotoImage(file = "imagens/person_50x50.png" )
 btnOpcao5 = Button(panel1, text = "Sair", image = imageIco2, compound=LEFT, relief = "sunken", 
                     width = 200, height = 70, font="calibri, 11",
                     command=window.destroy)
