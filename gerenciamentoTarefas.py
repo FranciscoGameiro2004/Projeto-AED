d5b874f302d314e14a4854575c13046e892de0e9
FranciscoGameiro2004 <40220426@esmad.ipp.pt>
2023-01-04 22:26:20 +0000

    Criação de funções básicas para gestão de tarefas.

---
+++ gerenciamentoTarefas.py
@@ -0,0 +1,36 @@
+def addTarefa(username, tarefa, descrição=''):
+    while True:
+        try:
+            ficheiroTarefas = open('files\\users\\{}\\listaTarefas.txt'.format(username), 'a', encoding='UTF-8')
+            break
+        except:
+            ficheiroTarefas = open('files\\users\\{}\\listaTarefas.txt'.format(username), 'c')
+            ficheiroTarefas.close()
+    
+    ficheiroTarefas.write('{};{}\n'.format(tarefa,descrição))
+    ficheiroTarefas.close()
+
+def delTarefa(username, numTar):
+    while True:
+        try:
+            ficheiroTarefas = open('files\\users\\{}\\listaTarefas.txt'.format(username), 'r+', encoding='UTF-8')
+            break
+        except:
+            ficheiroTarefas = open('files\\users\\{}\\listaTarefas.txt'.format(username), 'c')
+            ficheiroTarefas.close()
+
+    listaTarefas = ficheiroTarefas.readlines()
+    del listaTarefas[numTar]
+    
+    ficheiroTarefas.close()
+
+    ficheiroTarefas = open('files\\users\\{}\\listaTarefas.txt'.format(username), 'w', encoding='UTF-8')
+
+    for i in listaTarefas:
+        ficheiroTarefas.write(i)
+    
+    ficheiroTarefas.close()
+
+#addTarefa('admin','Dar mimo à Ufinha')
+
+#delTarefa('admin', 1)
